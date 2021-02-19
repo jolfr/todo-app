f5d261a3e7bc3a5b3f1ac00c341e446e6f6432fa
Jolfr <thomas.jack.carroll@gmail.com>
2021-02-19 10:46:50 -0500

    set up vue router

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+ <div id="dashboard">
+   <section>
+     <div class="col1">
+       <div class="profile">
+         <h5>n</h5>
+         </div>
+       </div>
+     <div class="col2">
+       <div>
+         <p class="no-results">There are currently no posts</p>
+       </div>
+     </div>
+   </section>
+ </div>
+</template>
+
+<script>
+export default {
+name: "Dahsboard"
+}
+</script>
+
+<style scoped>
+
+</style>
